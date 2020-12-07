e1f8d156b4af88f8e1a1f3ee035c2d6db7386ec7
komisuomi <only-for-serzh@yandex.ru>
2020-12-07 17:33:16 +0300

    TestHelper class Deleted

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,49 +1,57 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import lombok.val;
 import ru.netology.data.*;
 import ru.netology.page.CardBalanceAddPage;
+import ru.netology.page.LoginPage;
 import ru.netology.page.PersonAccountPage;
 
+import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneyTransferTest {
 
+    @BeforeEach
+    void openBrowser() { open("http://localhost:9999"); }
+
+    @BeforeEach
+    void sendLogin() {
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCode(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
     @Test
     void shouldTransferBetweenOwnCardsFromSecondToFirst() {
-        TestHelper.openBrowser();
-        TestHelper.sendLogin();
         val personAccount = new PersonAccountPage();
         val cardInfo = DataHelper.getCardInfo2();
-        int firstBalance = DataHelper.getBalance();
-        personAccount.validDeposit1();
-        val cardBalance = new CardBalanceAddPage();
+        val firstBalance = DataHelper.getBalance();
+        val cardBalance = personAccount.validDeposit1();
         cardBalance.transferMoney(cardInfo);
-        int newBalance = personAccount.returnBalance();
-        int expected = DataHelper.expectAmount(firstBalance, Integer.parseInt(CardBalanceAddPage.amountTransferred));
+        val newBalance = personAccount.returnBalance();
+        val expected = DataHelper.expectAmount(firstBalance, Integer.parseInt(CardBalanceAddPage.amountTransferred));
         assertEquals(expected, newBalance);
     }
 
     @Test
     void shouldTransferBetweenOwnCardsFromFirstToSecond() {
-        TestHelper.openBrowser();
-        TestHelper.sendLogin();
         val personAccount = new PersonAccountPage();
         val cardInfo = DataHelper.getCardInfo1();
-        int firstBalance = DataHelper.getOtherBalance();
+        val firstBalance = DataHelper.getOtherBalance();
         personAccount.validDeposit2();
         val cardBalance = new CardBalanceAddPage();
         cardBalance.transferMoney(cardInfo);
-        int newBalance = personAccount.returnOtherBalance();
-        int expected = DataHelper.expectAmount(firstBalance, Integer.parseInt(CardBalanceAddPage.amountTransferred));
+        val newBalance = personAccount.returnOtherBalance();
+        val expected = DataHelper.expectAmount(firstBalance, Integer.parseInt(CardBalanceAddPage.amountTransferred));
         assertEquals(expected, newBalance);
     }
 
     @Test
     void shouldMakeErrorWithEmptyField() {
-        TestHelper.openBrowser();
-        TestHelper.sendLogin();
         val personAccount = new PersonAccountPage();
         val cardInfo = DataHelper.getCardInfo1();
         personAccount.validDeposit2();
@@ -53,16 +61,14 @@ public class MoneyTransferTest {
 
     @Test
     void shouldTransferDoubleAmountBetweenOwnCardsFromFirstToSecond() {
-        TestHelper.openBrowser();
-        TestHelper.sendLogin();
         val personAccount = new PersonAccountPage();
         val cardInfo = DataHelper.getCardInfo1();
-        int firstBalance = DataHelper.getOtherBalance();
+        val firstBalance = DataHelper.getOtherBalance();
         personAccount.validDeposit2();
         val cardBalance = new CardBalanceAddPage();
         cardBalance.transferMoneyDoubleAmount(cardInfo);
-        int newBalance = personAccount.returnOtherBalance();
-        int expected = (int) (firstBalance - Double.parseDouble(DataHelper.shouldReturnDoubleAmount()));
+        val newBalance = personAccount.returnOtherBalance();
+        val expected = (int) (firstBalance - Double.parseDouble(DataHelper.shouldReturnDoubleAmount()));
         assertEquals(expected, newBalance);
     }
 }
